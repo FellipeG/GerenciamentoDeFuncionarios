c13207dc884be175f6fdf7339323bb6e7f210f8f
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-07-01 00:17:47 -0300

    Projeto concluído

---
+++ ExEmployee/Empregado.cs
@@ -0,0 +1,27 @@
+﻿namespace ExEmployee
+{
+    class Empregado
+    {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Empregado(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            porcentagem = Salario * porcentagem / 100;
+            Salario += porcentagem;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, R$ {Salario:F2}";
+        }
+    }
+}
---
+++ ExEmployee/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos empregados serão registrados? ");
+            int qtdEmpregados = int.Parse(Console.ReadLine());
+            List<Empregado> listaEmpregados = new List<Empregado>();
+
+            for(int i=1; i<=qtdEmpregados; i++)
+            {
+                Console.WriteLine($"Empregado #{i}:");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine());
+                listaEmpregados.Add(new Empregado(id, nome, salario));
+            }
+
+            Console.WriteLine("Digite o ID do empregado que terá o salário aumentado: ");
+            int idEmp = int.Parse(Console.ReadLine());
+            Empregado empregado = listaEmpregados.Find(emp => emp.Id == idEmp);
+            if (empregado != null)
+            {
+                Console.Write("Digite a porcentagem: ");
+                double porcentagem = double.Parse(Console.ReadLine());
+                empregado.AumentarSalario(porcentagem);
+            } else
+                Console.Write("Não existe nenhum empregado com essa ID!");
+            
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada de empregados:");
+            foreach(Empregado empreg in listaEmpregados)
+            {
+                Console.WriteLine(empreg);
+            }
+        }
+    }
+}
